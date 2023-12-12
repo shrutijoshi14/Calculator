cc11790d7752a6684ca68e53de03db0115f2a4de
Shruti Joshi <144242237+shrutijoshi14@users.noreply.github.com>
2023-12-12 23:40:09 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,16 @@
+let inputArea = document.querySelector( ".inputArea" );
+function insert ( num ) {
+    inputArea.value += num;
+}
+function equal () {
+    if ( inputArea ) {
+        inputArea.value = eval( inputArea.value );
+    }
+}
+function back () {
+    let exp = document.querySelector( ".inputArea" ).value;
+    inputArea.value = exp.substring( 0, exp.length - 1 );
+}
+function clean () {
+    inputArea = "";
+}
